8615fe8acd7f5697ec9a0f32a89097ff6f420744
MAZ <29178226+MAZ01001@users.noreply.github.com>
2024-05-01 20:36:23 +0200

    v2.0
    - fixed button falsely showing up when no README is displayed
    - added new button to select default collapse state
    - changed some styling
    - refactoring

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # Collapsible GitHub project files
 
-A [Tampermonkey](https://www.tampermonkey.net/ "official tampermonkey website") userscript to make the project files list on GitHub collapsible.
+A [Tampermonkey](https://www.tampermonkey.net/ "official tampermonkey website") userscript to make the project file lists on GitHub collapsible.
 
 >
 > Temporary solution to [`[Feature] Collapsible project files on Repository overview #109986`](https://github.com/orgs/community/discussions/109986 "open GitHub/community discussion 109986").
 >
-> And maybe also for [`toggle-files-button missing on "New Repository Overview" #6959`](https://github.com/refined-github/refined-github/issues/6959 "open GitHub/refined-github issue 6959") of [refined-github](https://github.com/refined-github/refined-github "GitHub: refined-github/refined-github").
+> And maybe also for [`toggle-files-button missing on "New Repository Overview" #6959`](https://github.com/refined-github/refined-github/issues/6959 "open GitHub/refined-github issue 6959")
+> of [refined-github](https://github.com/refined-github/refined-github "GitHub: refined-github/refined-github").
 >
 
-Stores the last state in `localStorage` to be persistent on `github.com` domain when reloading page/browser.
+Uses `localStorage` to be persistent on `github.com` domain when reloading page/browser.
 
-Now also shows the number of project files.
+Format: `show NUM rows // DEFAULT`
+
+- `show/hide NUM rows` will show/hide the list when clicked
+  - `NUM` displays the number of files/folders in the list
+- `DEFAULT` show the current default _collapse state_ and when clicked will cycle through the following options
+  - `auto` (default) only collapse when a README is displayed
+  - `expanded` never collapse
+  - `collapsed` allways collapse
+  - `last` uses last collapse state (starts collapsed)
 
 Click [here](https://github.com/MAZ01001/CollapsibleGitHubProjectFiles/raw/main/github_collapse.user.js "GitHub raw URL to github_collapse.user.js file") to see this userscript in tampermonkey (if you have the plugin installed).
--- github_collapse.user.js
+++ github_collapse.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         collapsible GitHub project files
-// @version      1.4
+// @version      2.0
 // @description  make GitHub project files collapsible
 // @author       MAZ / MAZ01001
 // @source       https://github.com/MAZ01001/CollapsibleGitHubProjectFiles
@@ -11,64 +11,101 @@
 
 (async function(){
     "use strict";
-    let table=document.querySelector("h2#folders-and-files+table>tbody"),
-        updatetimeout=NaN,
-        expand=localStorage.getItem("github_collapse",document.querySelector("div:has(nav a>span[data-content=README])+div>article")==null?"0":"1")==="0",
-        fileCount=0;
-    localStorage.setItem("github_collapse",expand?"0":"1");
     const
-        button=Object.assign(document.createElement("span"),{textContent:expand?"collapse ? project files":"expand ? project files",tabIndex:0,role:"button",title:"toggle list of project files"}),
-        tr=document.createElement("tr"),
-        td=Object.assign(document.createElement("td"),{colSpan:3});
-    button.style.cursor="pointer";
-    button.style.fontStyle="italic";
-    button.classList.add("Link--muted");
-    button.addEventListener("click",ev=>{
-        "use strict";
-        ev.preventDefault();
-        button.textContent=(expand=!expand)?`collapse ${fileCount} project files`:`expand ${fileCount} project files`;
-        localStorage.setItem("github_collapse",expand?"0":"1");
-        table.querySelectorAll("tr[id^=folder-row-]").forEach(v=>{v.style.display=expand?"":"none";});
-    },{passive:false});
-    td.style.textAlign="center";
-    td.style.paddingBlock=".2rem";
-    td.appendChild(button);
-    tr.appendChild(td);
-    const
-        tableObserver=new MutationObserver(()=>{
+        READMEarticle=()=>document.querySelector("div#readme:has(h2>a[href=\\#readme]) div>article")!=null||document.querySelector("div:has(nav a>span[data-content=README])+div>article")!=null,
+        RepoOverview=()=>document.querySelector("react-partial[partial-name=repos-overview]")!=null,
+        /**@type {(table:HTMLTableElement,button:HTMLSpanElement,collapse:boolean)=>void} for {@linkcode button_toggle}*/
+        Expand=(table,button,collapse)=>{
             "use strict";
             const rows=table.querySelectorAll("tr[id^=folder-row-]");
-            if(expand){
-                for(const row of rows)row.style.display="";
-                button.textContent=`collapse ${fileCount=rows.length} project files`;
-            }else{
+            if(collapse){
                 for(const row of rows)row.style.display="none";
-                button.textContent=`expand ${fileCount=rows.length} project files`;
+                button.textContent=`show ${rows.length} rows`;
+            }else{
+                for(const row of rows)row.style.display="";
+                button.textContent=`hide ${rows.length} rows`;
             }
+        },
+        /**@type {(auto:string)=>boolean} use {@linkcode auto}*/
+        LoadCollapse=auto=>{
+            "use strict";
+            let collapse=false;
+            localStorage.setItem("github_collapse_auto",auto);
+            switch(auto){
+                case"auto":collapse=READMEarticle();break;
+                case"expand":collapse=false;break;
+                case"collapse":collapse=true;break;
+                case"last":collapse=(localStorage.getItem("github_collapse")??"1")==="1";break;
+            }
+            localStorage.setItem("github_collapse",collapse?"1":"0");
+            return collapse;
+        };
+    let
+        table=document.querySelector("h2#folders-and-files+table>tbody"),
+        /**
+         * - `"auto"`: only collapse when a README is displayed
+         * - `"expand"`: never collapse
+         * - `"collapse"`: allways collapse
+         * - `"last"`: uses last collapse state (_starts collapsed_)
+         */
+        auto=localStorage.getItem("github_collapse_auto")??"auto",
+        collapse=LoadCollapse(auto),
+        updatetimeout=NaN;
+    const
+        button_toggle=Object.assign(document.createElement("span"),{tabIndex:0,role:"button",title:"toggle list of files & folders"}),
+        button_default=Object.assign(document.createElement("span"),{textContent:auto,tabIndex:0,role:"button",title:"select default collapse state: auto/expanded/collapsed/last"}),
+        tr=document.createElement("tr"),
+        td=Object.assign(document.createElement("td"),{colSpan:3}),
+        tableObserver=new MutationObserver(()=>{
+            "use strict";
+            Expand(table,button_toggle,collapse);
         }),
+        UpdateCallback=()=>{
+            "use strict";
+            if(!RepoOverview()&&!READMEarticle())return;
+            tableObserver.disconnect();
+            (table=document.querySelector("h2#folders-and-files+table>tbody")).querySelector("tr#folder-row-0").insertAdjacentElement("beforebegin",tr);
+            Expand(table,button_toggle,collapse=LoadCollapse(auto));
+            tableObserver.observe(table,{childList:true,subtree:true});
+        },
         bodyObserver=new MutationObserver(mutations=>{
             "use strict";
-            if(mutations.some(v=>v.target.contains(table)))return;
+            if(mutations.some(v=>table.contains(v.target)))return;
             clearTimeout(updatetimeout);
-            updatetimeout=setTimeout(()=>{
-                "use strict";
-                if(document.querySelector("react-partial[partial-name=repos-overview]")==null)return;
-                (table=document.querySelector("h2#folders-and-files+table>tbody")).querySelector("tr:first-of-type").insertAdjacentElement("afterend",tr);
-                tableObserver.disconnect();
-                tableObserver.observe(table,{childList:true,subtree:true});
-            },0);
+            updatetimeout=setTimeout(UpdateCallback,0);
         });
-    bodyObserver.observe(document.body,{childList:true,subtree:true});
-    if(table!=null){
-        table.querySelector("tr:first-of-type").insertAdjacentElement("afterend",tr);
-        const rows=table.querySelectorAll("tr[id^=folder-row-]");
-        if(expand){
-            for(const row of rows)row.style.display="";
-            button.textContent=`collapse ${fileCount=rows.length} project files`;
-        }else{
-            for(const row of rows)row.style.display="none";
-            button.textContent=`expand ${fileCount=rows.length} project files`;
+    button_toggle.style.cursor="pointer";
+    button_toggle.classList.add("Link--muted");
+    button_toggle.addEventListener("click",ev=>{
+        "use strict";
+        ev.preventDefault();
+        localStorage.setItem("github_collapse",(collapse=!collapse)?"1":"0");
+        Expand(table,button_toggle,collapse);
+    },{passive:false});
+    button_default.style.cursor="pointer";
+    button_default.classList.add("Link--muted");
+    button_default.addEventListener("click",ev=>{
+        "use strict";
+        ev.preventDefault();
+        switch(auto){
+            case"auto":auto="expand";break;
+            case"expand":auto="collapse";break;
+            case"collapse":auto="last";break;
+            case"last":auto="auto";break;
         }
+        localStorage.setItem("github_collapse_auto",button_default.textContent=auto);
+    },{passive:false});
+    td.style.textAlign="center";
+    td.style.fontStyle="italic";
+    td.style.paddingBlock=".2rem";
+    td.style.borderTop="1px solid var(--borderColor-default, var(--color-border-default))";
+    td.style.borderInline="4px solid var(--borderColor-default, var(--color-border-default))";
+    td.append(button_toggle," // ",button_default);
+    tr.appendChild(td);
+    if(table!=null&&(RepoOverview()||READMEarticle())){
+        table.querySelector("tr#folder-row-0").insertAdjacentElement("beforebegin",tr);
+        Expand(table,button_toggle,collapse=LoadCollapse(auto));
         tableObserver.observe(table,{childList:true,subtree:true});
     }
+    bodyObserver.observe(document.body,{childList:true,subtree:true});
 })();
